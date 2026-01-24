60e8cbb50a7f24fd76b191d4803b8a23f20384d9
__ <__@__>
2026-01-24 17:42:02 +0100

    Follow dotnvim changes

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,47 +1,39 @@
-local configDir = vim.fn.expand('<sfile>:p:h:h')
+vim.g.pluginDirs = vim.iter({
+  'arctgx',
+  'plugins',
+  'unmerged',
+  'colorscheme',
+}):map(function (group)
+  return vim.fs.joinpath(vim.fn.stdpath('config'), 'pack', group, 'opt')
+end):totable()
 
-vim.env['XDG_CONFIG_HOME'] = configDir
-vim.env['XDG_DATA_HOME'] = configDir .. '/.xdg/data'
-vim.env['XDG_STATE_HOME'] = configDir .. '/.xdg/state'
-vim.env['XDG_CACHE_HOME'] = configDir .. '/.xdg/cache'
-local stdPathConfig = vim.fn.stdpath('config')
+for _, path in ipairs(vim.opt.packpath:get()) do
+  if vim.startswith(path, '/etc/') or vim.startswith(path, '/usr/') then
+    vim.opt.packpath:remove(path)
+  end
+end
 
-vim.opt.runtimepath:prepend(stdPathConfig)
-vim.opt.packpath:prepend(stdPathConfig)
-vim.opt.runtimepath:append(stdPathConfig .. '/after')
+for _, path in ipairs(vim.opt.runtimepath:get()) do
+  if vim.startswith(path, '/etc/') or vim.startswith(path, '/usr/') then
+    vim.opt.runtimepath:remove(path)
+  end
+end
 
-vim.g.pluginDirs = {
-  vim.fn.expand(stdPathConfig .. '/pack/unmerged/opt'),
-  vim.fn.expand(stdPathConfig .. '/pack/bundle/opt'),
-  vim.fn.expand(stdPathConfig .. '/pack/colorscheme/opt'),
-  vim.fn.expand(stdPathConfig .. '/pack/arctgx/opt'),
-}
-vim.g.initialVimDirectory = stdPathConfig
+vim.loader.enable()
 
+---@type NvimPlugins
 local extensions = {
-  -- {name = 'plenary.nvim'},
 }
 
-for _, config in ipairs(extensions) do
-  local bang = true
-  if config.bang ~= nil then
-    bang = config.bang
-  end
-  local ok, error = pcall(vim.cmd.packadd, {
-    args = {config.name},
-    bang = bang,
-  })
+if not vim.tbl_contains({vim.fn.stdpath('config'), vim.env.NVIM_UNSANDBOXED_CONFIGDIR}, vim.uv.cwd()) then
+  local exrc = vim.fs.joinpath(vim.fn.stdpath('config'), '.nvim.local.lua')
+  local ok = pcall(vim.uv.fs_stat, exrc)
   if not ok then
-    vim.api.nvim_create_autocmd('UIEnter', {
-      once = true,
-      callback = function()
-        vim.notify(error, vim.log.levels.ERROR)
-      end,
-    })
+    return
   end
+  assert(loadstring(vim.secure.read(exrc) or '', 'Cannot load Lua script from ' .. exrc))()
 end
 
-local initPerHost = vim.g.initialVimDirectory .. '/initPerHost.lua'
-if vim.fn.filereadable(initPerHost) == 1 then
-  dofile(initPerHost)
-end
+require('dotnvim.plugin').packadd(extensions)
+
+vim.go.exrc = true
